92bee0a0f29990058b0e739ebdb9e4c5e541cdc5
Ignacio Porte <ignacio.porte@buda.com>
2024-06-04 00:43:13 -0400

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<div
+  class="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800"
+>
+  <div class="text-7xl mb-4">🚧👷‍♂️🔧</div>
+  <h1 class="text-9xl font-bold mb-2">404</h1>
+  <p class="text-2xl mb-4">Oops! The page you're looking for doesn't exist.</p>
+  <div class="text-7xl mb-4">🙈🛠️😕</div>
+  <a href="/" class="text-lg text-blue-500 hover:underline"
+    >Go back to the homepage</a
+  >
+</div>
